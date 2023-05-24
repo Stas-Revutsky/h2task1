2a1bd41751997e89ce95cc9783595828a5148fb9
Admin <stas.revutsky@gmail.com>
2023-05-24 21:23:45 +0300

    Add project files.

---
+++ h2task1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace h2task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float topCap = 5;
+            float bottomCap = -5;
+
+            Console.WriteLine($"Enter a number between {topCap} and {bottomCap}");
+            float firstNumber = float.Parse(Console.ReadLine());
+            Console.WriteLine($"Enter a number between {topCap} and {bottomCap}");
+            float secondNumber = float.Parse(Console.ReadLine());
+            Console.WriteLine($"Enter a number between {topCap} and {bottomCap}");
+            float thirdNumber = float.Parse(Console.ReadLine());
+
+            if (firstNumber <= topCap && secondNumber <= topCap && thirdNumber <= topCap 
+                && firstNumber >= bottomCap && secondNumber >= bottomCap && thirdNumber >= bottomCap) 
+            {
+                Console.WriteLine($"The numbers are in range of {topCap} to {bottomCap}");
+            }
+            else
+            {
+                Console.WriteLine("The numbers are not in range");
+            }
+            
+        }
+    }
+}
